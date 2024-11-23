1cf82efb8b61758c61a423d71b7b9e169cf232d4
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-11-23 16:54:24 +0300

    Lecture 10 - Business Abstract Serviceler

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        void TInsert(T entity);
+        void TDelete(T entity);
+        void TUpdate(T entity);
+        List<T> TGetListAll();
+        List<T> TGetListWhere(Expression<Func<T, bool>> predicate);
+        T TGetById(int id);
+    }
+}
