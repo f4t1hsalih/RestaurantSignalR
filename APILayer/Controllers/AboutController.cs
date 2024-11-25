83aa196b573ec6284fff38735d5feef021f11bf4
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-11-25 09:09:09 +0300

    Lecture 17 - About Api Controller Yazılması

---
+++ APILayer/Controllers/AboutController.cs
@@ -0,0 +1,67 @@
+﻿using BusinessLayer.Abstract;
+using DTOLayer.AboutDto;
+using EntityLayer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APILayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        private readonly IAboutService _aboutService;
+        public AboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+
+        [HttpGet]
+        public IActionResult AboutList()
+        {
+            var value = _aboutService.TGetListAll();
+            return Ok(value);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAboutByID(int id)
+        {
+            var about = _aboutService.TGetById(id);
+            return Ok(about);
+        }
+
+        [HttpPost]
+        public IActionResult InsertAbout(InsertAboutDto insertAboutDto)
+        {
+            About about = new About
+            {
+                ImageUrl = insertAboutDto.ImageUrl,
+                Title = insertAboutDto.Title,
+                Description = insertAboutDto.Description
+            };
+            _aboutService.TInsert(about);
+            return Ok("Kayıt Başarıyla Eklendi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateAbout(UpdateAboutDto updateAboutDto)
+        {
+            About about = new About
+            {
+                AboutId = updateAboutDto.AboutId,
+                ImageUrl = updateAboutDto.ImageUrl,
+                Title = updateAboutDto.Title,
+                Description = updateAboutDto.Description
+            };
+            _aboutService.TUpdate(about);
+            return Ok("Kayıt Başarıyla Güncellendi");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAbout(int id)
+        {
+            var about = _aboutService.TGetById(id);
+            _aboutService.TDelete(about);
+            return Ok("Kayıt Başarıyla Silindi");
+        }
+    }
+}
