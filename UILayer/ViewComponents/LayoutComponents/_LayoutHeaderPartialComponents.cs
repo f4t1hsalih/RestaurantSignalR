fc3d95714866c232236ab803bb61b815d624e082
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-11-26 17:49:44 +0300

    Lecture 28 - Header Layout

---
+++ UILayer/ViewComponents/LayoutComponents/_LayoutHeaderPartialComponents.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.ViewComponents.LayoutComponents
+{
+    public class _LayoutHeaderPartialComponents : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
