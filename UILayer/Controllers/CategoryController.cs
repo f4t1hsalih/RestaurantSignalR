d2bbb222562d0a8171aa2a8b2b342206f742318e
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-11-27 20:50:52 +0300

    Lecture 32 - Kategori Listesi Api Consume

--- UILayer/Controllers/CategoryController.cs
+++ UILayer/Controllers/CategoryController.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UILayer.DTO.CategoryDTO;
 
 namespace UILayer.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7068/api/Category");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var categories = JsonConvert.DeserializeObject<List<CategoryResultDTO>>(jsonData);
+                return View(categories);
+            }
             return View();
         }
     }
---
+++ UILayer/DTO/CategoryDTO/CategoryResultDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace UILayer.DTO.CategoryDTO
+{
+    public class CategoryResultDTO
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
