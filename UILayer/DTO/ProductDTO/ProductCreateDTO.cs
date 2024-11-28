c6cdaa517f9647621f478b978a135af2e4f528ca
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-11-28 19:55:18 +0300

    Lecture 37 - Ürün Girişi Sayfasının Tasarlanması

--- UILayer/DTO/ProductDTO/ProductCreateDTO.cs
+++ UILayer/DTO/ProductDTO/ProductCreateDTO.cs
@@ -6,7 +6,7 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
-        public bool Status { get; set; }
+        public bool Status { get; set; } = true;
         public int CategoryId { get; set; }
     }
 }
