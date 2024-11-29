468bf13bda28ad174714ee9fddadc80b9b453017
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-11-29 17:17:04 +0300

    Lecture 42 - Rezervasyon İşlemleri

---
+++ UILayer/Controllers/BookingController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using UILayer.DTO.BookingDTO;
+
+namespace UILayer.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BookingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7068/api/Booking");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var bookings = JsonConvert.DeserializeObject<List<BookingResultDTO>>(jsonData);
+                return View(bookings);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateBooking()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateBooking(BookingCreateDTO booking)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var json = JsonConvert.SerializeObject(booking);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7068/api/Booking", data);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteBooking(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.DeleteAsync($"https://localhost:7068/api/Booking/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateBooking(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7068/api/Booking/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var booking = JsonConvert.DeserializeObject<BookingUpdateDTO>(jsonData);
+                return View(booking);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateBooking(BookingUpdateDTO booking)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var json = JsonConvert.SerializeObject(booking);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:7068/api/Booking/", data);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
