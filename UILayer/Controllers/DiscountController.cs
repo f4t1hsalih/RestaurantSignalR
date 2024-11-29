3de23ea5d8aa283543ec9d72ca59a0179772fc56
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-11-29 17:52:17 +0300

    Lecture 44 - Discount İşlemleri

---
+++ UILayer/Controllers/DiscountController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using UILayer.DTO.DiscountDTO;
+
+namespace UILayer.Controllers
+{
+    public class DiscountController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public DiscountController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7068/api/Discount");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var discounts = JsonConvert.DeserializeObject<List<DiscountResultDTO>>(jsonData);
+                return View(discounts);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateDiscount()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateDiscount(DiscountCreateDTO discount)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var json = JsonConvert.SerializeObject(discount);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7068/api/Discount", data);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteDiscount(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.DeleteAsync($"https://localhost:7068/api/Discount/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateDiscount(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7068/api/Discount/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var discount = JsonConvert.DeserializeObject<DiscountUpdateDTO>(jsonData);
+                return View(discount);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateDiscount(DiscountUpdateDTO discount)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var json = JsonConvert.SerializeObject(discount);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:7068/api/Discount/", data);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
