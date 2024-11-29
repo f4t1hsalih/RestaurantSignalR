bb5cb4cab08cd7cb2d462eb8736bda3e30ce26b2
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-11-29 19:35:14 +0300

    Lecture 46 - SocialMedia İşlemleri

---
+++ UILayer/Controllers/SocialMediaController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using UILayer.DTO.SocialMediaDTO;
+
+namespace UILayer.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public SocialMediaController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7068/api/SocialMedia");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var socialMedias = JsonConvert.DeserializeObject<List<SocialMediaResultDTO>>(jsonData);
+                return View(socialMedias);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateSocialMedia(SocialMediaCreateDTO socialMedia)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var json = JsonConvert.SerializeObject(socialMedia);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7068/api/SocialMedia", data);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteSocialMedia(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.DeleteAsync($"https://localhost:7068/api/SocialMedia/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateSocialMedia(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7068/api/SocialMedia/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var socialMedia = JsonConvert.DeserializeObject<SocialMediaUpdateDTO>(jsonData);
+                return View(socialMedia);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateSocialMedia(SocialMediaUpdateDTO socialMedia)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var json = JsonConvert.SerializeObject(socialMedia);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:7068/api/SocialMedia/", data);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
