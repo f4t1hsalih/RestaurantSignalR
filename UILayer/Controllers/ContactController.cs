a1c050f0ba01002137714b49aaf06eaeaa1d9cbf
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-11-29 17:31:32 +0300

    Lecture 43 - Contact İşlemleri

---
+++ UILayer/Controllers/ContactController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using UILayer.DTO.ContactDTO;
+
+namespace UILayer.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7068/api/Contact");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var contacts = JsonConvert.DeserializeObject<List<ContactResultDTO>>(jsonData);
+                return View(contacts);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateContact(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://localhost:7068/api/Contact/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var contact = JsonConvert.DeserializeObject<ContactUpdateDTO>(jsonData);
+                return View(contact);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateContact(ContactUpdateDTO contact)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var json = JsonConvert.SerializeObject(contact);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:7068/api/Contact/", data);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
