4299d4e3b7a78d4e928d198d5144a108fbdb88b3
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-11-30 21:29:14 +0300

    Lecture 54 - Kategori SSayısını Getirme Hazırlığı

--- APILayer/Hubs/SignalRHub.cs
+++ APILayer/Hubs/SignalRHub.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.SignalR;
 
 namespace APILayer.Hubs
 {
     public class SignalRHub : Hub
     {
+        Context context = new Context();
+
+        public async Task SendCategoryCount()
+        {
+            var categoryCount = context.Categories.Count();
+            await Clients.All.SendAsync("ReceiveCategoryCount", categoryCount);
+        }
     }
 }
