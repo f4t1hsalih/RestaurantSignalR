701020d609719ac4cb9ef8bc25e41331c65dcaa0
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-03 17:35:25 +0300

    Lecture 57 - İstatistik Controller ve View

---
+++ UILayer/Controllers/StatistikController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.Controllers
+{
+    public class StatistikController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
