66e3454d97fbbce7d922824562ba37ddc9bb29f4
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-04 16:31:32 +0300

    Lecture 67 - Kasa Tablosunun Oluşturulması

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -20,6 +20,7 @@ namespace DataAccessLayer.Concrete
         public DbSet<Testimonial> Testimonials { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<MoneyCase> MoneyCases { get; set; }
 
     }
 }
