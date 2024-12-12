111e1cdd88ab912c7e092b0cdf9679b708e50053
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-12 18:46:24 +0300

    Lecture 80 - Küçük Bir Refactor

--- APILayer/Hubs/SignalRHub.cs
+++ APILayer/Hubs/SignalRHub.cs
@@ -14,24 +14,17 @@ namespace APILayer.Hubs
             _productService = productService;
         }
 
-        public async Task SendCategoryCount()
+        public async Task SendStatistics()
         {
             var categoryCount = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount", categoryCount);
-        }
-        public async Task SendActiveCategoryCount()
-        {
+
             var activeCategoryCount = _categoryService.TActiveCategoryCount();
             await Clients.All.SendAsync("ReceiveActiveCategoryCount", activeCategoryCount);
-        }
-        public async Task SendPassiveCategoryCount()
-        {
+
             var passiveCategoryCount = _categoryService.TPassiveCategoryCount();
             await Clients.All.SendAsync("ReceivePassiveCategoryCount", passiveCategoryCount);
-        }
 
-        public async Task SendActiveProductCount()
-        {
             var activeProductCount = _productService.TActiveProductCount();
             await Clients.All.SendAsync("ReceiveActiveProductCount", activeProductCount);
         }
