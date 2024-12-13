ccc0a3e3667a5d7a17caf43bbbaf36551da0fd03
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-13 19:25:58 +0300

    Lecture 92 - Ana Sayfa Hakkımda ve Menü Listesi

---
+++ UILayer/ViewComponents/HomePageCompanents/_HomePageFoods.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.ViewComponents.HomePageCompanents
+{
+    public class _HomePageFoods : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
