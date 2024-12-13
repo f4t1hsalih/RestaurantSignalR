16b7d417f1fc6da322e697dfe9d8d1aae09bd252
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-13 17:15:14 +0300

    Lecture 88-89 - UI Layout

---
+++ UILayer/ViewComponents/UILayoutComponents/_UILayoutHeader.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutHeader : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ UILayer/ViewComponents/UILayoutComponents/_UILayoutNavbar.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutNavbar : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
