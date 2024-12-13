56b1a7dd83c42689f4fcef60d43f89bbab5d46f1
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-13 18:13:55 +0300

    Lecture 90-91 - Slider ve Offer Alanları

---
+++ UILayer/ViewComponents/HomePageCompanents/_HomePageSlider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.ViewComponents.HomePageCompanents
+{
+    public class _HomePageSlider : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
