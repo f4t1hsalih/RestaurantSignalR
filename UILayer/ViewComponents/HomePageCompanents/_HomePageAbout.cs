78b03d54e93dac3967eaee5724849f0b412ab136
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-25 18:55:59 +0300

    Lecture 98 - Hakkımızda kısmı ve referanslar kısmı api consumeleri

--- UILayer/ViewComponents/HomePageCompanents/_HomePageAbout.cs
+++ UILayer/ViewComponents/HomePageCompanents/_HomePageAbout.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UILayer.DTO.AboutDTO;
 
 namespace UILayer.ViewComponents.HomePageCompanents
 {
     public class _HomePageAbout : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _HomePageAbout(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7068/api/About");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var about = JsonConvert.DeserializeObject<List<AboutResultDTO>>(jsonData);
+                return View(about);
+            }
             return View();
         }
     }
--- UILayer/ViewComponents/HomePageCompanents/_HomePageTestimonial.cs
+++ UILayer/ViewComponents/HomePageCompanents/_HomePageTestimonial.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UILayer.DTO.TestimonialDTO;
 
 namespace UILayer.ViewComponents.HomePageCompanents
 {
     public class _HomePageTestimonial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _HomePageTestimonial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7068/api/Testimonial");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var testimonials = JsonConvert.DeserializeObject<List<TestimonialResultDTO>>(jsonData);
+                return View(testimonials);
+            }
             return View();
         }
     }
