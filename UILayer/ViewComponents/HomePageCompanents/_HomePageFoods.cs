2282a07247dca0e77b79053e9a6d75b8f17cc3a7
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-25 18:40:20 +0300

    Lecture 97 - Ana Sayfa Menü Consume İşlemi

--- UILayer/ViewComponents/HomePageCompanents/_HomePageFoods.cs
+++ UILayer/ViewComponents/HomePageCompanents/_HomePageFoods.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UILayer.DTO.ProductDTO;
 
 namespace UILayer.ViewComponents.HomePageCompanents
 {
     public class _HomePageFoods : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _HomePageFoods(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7068/api/Product");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var foods = JsonConvert.DeserializeObject<List<ProductResultDTO>>(jsonData);
+                return View(foods);
+            }
             return View();
         }
     }
