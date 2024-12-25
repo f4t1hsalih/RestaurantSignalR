191d042f7481bf73a00556003cb6e6c9814ee9b4
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-25 17:24:25 +0300

    Lecture 95 - Slider Api'nin Consume Edilmesi

--- UILayer/Program.cs
+++ UILayer/Program.cs
@@ -23,6 +23,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
--- UILayer/ViewComponents/HomePageCompanents/_HomePageSlider.cs
+++ UILayer/ViewComponents/HomePageCompanents/_HomePageSlider.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UILayer.DTO.SliderDTO;
 
 namespace UILayer.ViewComponents.HomePageCompanents
 {
     public class _HomePageSlider : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _HomePageSlider(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7068/api/Slider");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var sliders = JsonConvert.DeserializeObject<List<SliderResultDTO>>(jsonData);
+                return View(sliders);
+            }
             return View();
         }
+
     }
 }
