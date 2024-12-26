c052fe9f357310e8aebad405f7be2e8c1f3dfceb
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-26 17:57:11 +0300

    Lecture 107 - Sepet Ekranı Tasarımı

---
+++ UILayer/Controllers/BasketController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UILayer.DTO.BasketDTO;
+
+namespace UILayer.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BasketController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7068/api/Basket?tableNumber");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var basket = JsonConvert.DeserializeObject<List<BasketResultDTO>>(jsonData);
+                return View(basket);
+            }
+            return View();
+        }
+    }
+}
