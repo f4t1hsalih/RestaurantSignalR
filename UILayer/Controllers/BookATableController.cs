28ab9bc2b0727ad6886114c1f1ddfa5eaedae471
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-27 17:47:27 +0300

    Lecture 122 - Rezervasyon Sayfası

---
+++ UILayer/Controllers/BookATableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.Controllers
+{
+    public class BookATableController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
