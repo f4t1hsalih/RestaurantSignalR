87124e3ef48b3a3a65e1f77f3c970a06dd0e1971
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-30 18:04:45 +0300

    Lecture 151 - Chat.js kısmını tamamlanması

--- UILayer/wwwroot/js/chat.js
+++ UILayer/wwwroot/js/chat.js
@@ -17,3 +17,18 @@ connection.on("ReceiveMessage", function (user, message) {
     li.innerHTML += ` :${message} - ${currentHour}:${currentMinute}`;
     document.getElementById("messagelist").appendChild(li);
 });
+
+connection.start().then(function () {
+    document.getElementById("sendbutton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendbutton").addEventListener("click", function (event) {
+    var user = document.getElementById("userinput").value;
+    var message = document.getElementById("messageinput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
