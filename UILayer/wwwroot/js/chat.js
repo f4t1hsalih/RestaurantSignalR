083cbb299747caa888119bcc63ee55522b20a3fb
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2024-12-30 17:57:52 +0300

    Lecture 150 - Chat.js ilk kısım

---
+++ UILayer/wwwroot/js/chat.js
@@ -0,0 +1,19 @@
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("https://localhost:7068/SignalRHub")
+    .build();
+
+document.getElementById("sendbutton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var currentTime = new Date();
+    var currentHour = currentTime.getHours();
+    var currentMinute = currentTime.getMinutes();
+
+    var li = document.createElement("li");
+    var span = document.createElement("span");
+    span.style.fontWeight = "bold";
+    span.textContent = user;
+    li.appendChild(span);
+    li.innerHTML += ` :${message} - ${currentHour}:${currentMinute}`;
+    document.getElementById("messagelist").appendChild(li);
+});
