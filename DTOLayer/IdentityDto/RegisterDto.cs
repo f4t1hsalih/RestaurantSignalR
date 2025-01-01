447770e91c9622636766f7317e36484f70388f41
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2025-01-01 15:35:49 +0300

    Lecture 158 - Register Backend İşlemi

---
+++ DTOLayer/IdentityDto/RegisterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace DTOLayer.IdentityDto
+{
+    public class RegisterDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ UILayer/Controllers/RegisterController.cs
@@ -0,0 +1,40 @@
+﻿using EntityLayer.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using DTOLayer.IdentityDto;
+
+namespace UILayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(RegisterDto registerDto)
+        {
+            var appUser = new AppUser()
+            {
+                Name = registerDto.Name,
+                Surname = registerDto.Surname,
+                Email = registerDto.Email,
+                UserName = registerDto.Username
+            };
+            var result = await _userManager.CreateAsync(appUser, registerDto.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
