48c5b990a4f4eceffc1dca9f141ab260496faf05
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2025-01-03 19:28:18 +0300

    Lecture 180 - Sosyal medya linklerinin listelenmesi

---
+++ UILayer/ViewComponents/UILayoutComponents/_UILayoutSocialMedia.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UILayer.DTO.SocialMediaDTO;
+
+namespace UILayer.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutSocialMedia : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _UILayoutSocialMedia(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7068/api/SocialMedia");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var contacts = JsonConvert.DeserializeObject<List<SocialMediaResultDTO>>(jsonData);
+                return View(contacts);
+            }
+            return View();
+        }
+    }
+}
