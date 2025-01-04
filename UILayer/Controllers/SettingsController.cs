20b2f0a6e1f39b1bdb11abc9886f51323a6f81d6
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2025-01-04 17:48:41 +0300

    Lecture 192 - Bilgileri güncelleme sayfasına getirme

---
+++ UILayer/Controllers/SettingsController.cs
@@ -0,0 +1,30 @@
+﻿using EntityLayer.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UILayer.DTO.IdentityDTO;
+
+namespace UILayer.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public SettingsController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var value = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditDTO userEditDTO = new UserEditDTO
+            {
+                Name = value.Name,
+                Surname = value.Surname,
+                Username = value.UserName,
+                EMail = value.Email
+            };
+            return View(userEditDTO);
+        }
+    }
+}
