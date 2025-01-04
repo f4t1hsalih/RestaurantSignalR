8aa39a1ef56f61afb5da9ca19ab428b35ae6ca9e
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2025-01-04 18:19:25 +0300

    Lecture 194 - Mail gönderme backend

---
+++ UILayer/Controllers/MailController.cs
@@ -0,0 +1,42 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using UILayer.DTO.MailDTO;
+
+namespace UILayer.Controllers
+{
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(MailCreateDTO mailCreateDTO)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("SignalR Rezervasyon", "mail adresi");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailCreateDTO.ReceiverEMail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = mailCreateDTO.Message;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = mailCreateDTO.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com", 587, true);
+            client.Authenticate("mail adresi", "key");
+
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            return RedirectToAction("Index", "Statistik");
+        }
+    }
+}
---
+++ UILayer/DTO/MailDTO/MailCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace UILayer.DTO.MailDTO
+{
+    public class MailCreateDTO
+    {
+        public string ReceiverEMail { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
