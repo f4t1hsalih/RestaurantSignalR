81f99b7c437107abeae917e8c50858290cbfab0a
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2025-01-06 16:05:59 +0300

    Lecture 227 - Rapid Api üzerinden kodların çekilmesi

--- UILayer/Controllers/RapidApiController.cs
+++ UILayer/Controllers/RapidApiController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UILayer.DTO.RapidApiDTO;
 
 namespace UILayer.Controllers
 {
     public class RapidApiController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=60&tags=under_30_minutes"),
+                Headers =
+    {
+        { "x-rapidapi-key", "341c15bda0mshefa47b42aad83e6p1a5a46jsnb48a81a706cf" },
+        { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultTastyApi>(body);
+                return View(values);
+            }
         }
     }
 }
---
+++ UILayer/DTO/RapidApiDTO/ResultTastyApi.cs
@@ -0,0 +1,11 @@
+﻿namespace UILayer.DTO.RapidApiDTO
+{
+    public class ResultTastyApi
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string original_video_url { get; set; }
+        public int total_time_minutes { get; set; }
+        public string thumbnail_url { get; set; }
+    }
+}
