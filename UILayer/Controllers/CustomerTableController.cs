efd0c868ca6499ea716a70f8f0de2b6cb4647a03
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2025-01-08 19:02:19 +0300

    Lecture 236 237 - CustomerTableList sayfası

---
+++ UILayer/Controllers/CustomerTableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.Controllers
+{
+    public class CustomerTableController : Controller
+    {
+        public IActionResult CustomerTableList()
+        {
+            return View();
+        }
+    }
+}
