601850bf7f1dbc8b0aa3db667e24ca1f05a8e17f
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2025-01-08 19:18:40 +0300

    Lecture 238 - Masaların UI'da listelenmesi

--- UILayer/Controllers/CustomerTableController.cs
+++ UILayer/Controllers/CustomerTableController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UILayer.DTO.TableDTO;
 
 namespace UILayer.Controllers
 {
     public class CustomerTableController : Controller
     {
-        public IActionResult CustomerTableList()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CustomerTableController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> CustomerTableList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7068/api/Table");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var categories = JsonConvert.DeserializeObject<List<TableResultDTO>>(jsonData);
+                return View(categories);
+            }
             return View();
         }
     }
