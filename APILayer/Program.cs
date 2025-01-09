492263bfe69a8205ae558bcb6ee7b166578cde61
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2025-01-09 19:25:56 +0300

    Lecture 250 - Extension Sınıf

--- APILayer/Program.cs
+++ APILayer/Program.cs
@@ -1,21 +1,5 @@
 using APILayer.Hubs;
-using BusinessLayer.Abstract;
-using BusinessLayer.Concrete;
-using BusinessLayer.ValidationRules.AboutValidations;
-using BusinessLayer.ValidationRules.BookingValidations;
-using BusinessLayer.ValidationRules.CategoryValidations;
-using BusinessLayer.ValidationRules.ContactValidations;
-using BusinessLayer.ValidationRules.DiscountValidations;
-using BusinessLayer.ValidationRules.IdentityValidations;
-using BusinessLayer.ValidationRules.NotificationValidations;
-using BusinessLayer.ValidationRules.ProductValidations;
-using BusinessLayer.ValidationRules.SliderValidations;
-using BusinessLayer.ValidationRules.SocialMediaValidations;
-using BusinessLayer.ValidationRules.TableValidations;
-using BusinessLayer.ValidationRules.TestimonialValidations;
-using DataAccessLayer.Abstract;
-using DataAccessLayer.Concrete;
-using DataAccessLayer.EntityFramework;
+using BusinessLayer.Container;
 using FluentValidation;
 using System.Reflection;
 using System.Text.Json.Serialization;
@@ -34,76 +18,16 @@ builder.Services.AddCors(options =>
 });
 builder.Services.AddSignalR();
 
-builder.Services.AddDbContext<Context>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
-builder.Services.AddScoped<IAboutService, AboutManager>();
-builder.Services.AddScoped<IAboutDal, EfAboutDal>();
-
-builder.Services.AddScoped<IBookingService, BookingManager>();
-builder.Services.AddScoped<IBookingDal, EfBookingDal>();
-
-builder.Services.AddScoped<ICategoryService, CategoryManager>();
-builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
-
-builder.Services.AddScoped<IContactService, ContactManager>();
-builder.Services.AddScoped<IContactDal, EfContactDal>();
-
-builder.Services.AddScoped<IDiscountService, DiscountManager>();
-builder.Services.AddScoped<IDiscountDal, EfDiscountDal>();
-
-builder.Services.AddScoped<IProductService, ProductManager>();
-builder.Services.AddScoped<IProductDal, EfProductDal>();
-
-builder.Services.AddScoped<ISocialMediaService, SocialMediaManager>();
-builder.Services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
-
-builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
-builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
-
-builder.Services.AddScoped<IOrderService, OrderManager>();
-builder.Services.AddScoped<IOrderDal, EfOrderDal>();
-
-builder.Services.AddScoped<IMoneyCaseService, MoneyCaseManager>();
-builder.Services.AddScoped<IMoneyCaseDal, EfMoneyCaseDal>();
-
-builder.Services.AddScoped<ITableService, TableManager>();
-builder.Services.AddScoped<ITableDal, EfTableDal>();
-
-builder.Services.AddScoped<ISliderService, SliderManager>();
-builder.Services.AddScoped<ISliderDal, EfSliderDal>();
-
-builder.Services.AddScoped<IBasketService, BasketManager>();
-builder.Services.AddScoped<IBasketDal, EfBasketDal>();
-
-builder.Services.AddScoped<INotificationService, NotificationManager>();
-builder.Services.AddScoped<INotificationDal, EfNotificationDal>();
-
-builder.Services.AddValidatorsFromAssemblyContaining<AboutUpdateValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<BookingAddValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<BookingUpdateValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<CategoryAddValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<CategoryUpdateValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<ContactUpdateValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<DiscountAddValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<DiscountUpdateValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<RegisterValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<NotificationAddValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<NotificationUpdateValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<ProductAddValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<ProductUpdateValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<SliderUpdateValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<SocialMediaAddValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<SocialMediaUpdateValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<TableAddValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<TableUpdateValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<TestimonialAddValidation>();
-builder.Services.AddValidatorsFromAssemblyContaining<TestimonialUpdateValidation>();
+//Baðýmlýlýklar Container Klasöründen Çaðrýldý
+builder.Services.ContainerDependencies();
 
 // json tarafýnda iç içe yapý hatasýný engeller
 builder.Services.AddControllersWithViews().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
-// Add services to the container.
+// Burada FluentValidation kütüphanesini kullanarak model validasyonlarýný yapabilmek için gerekli ayarlamalarý yapýyoruz.
+ValidatorOptions.Global.LanguageManager.Culture = new System.Globalization.CultureInfo("tr");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ BusinessLayer/Container/Extensions.cs
@@ -0,0 +1,93 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules.AboutValidations;
+using BusinessLayer.ValidationRules.BookingValidations;
+using BusinessLayer.ValidationRules.CategoryValidations;
+using BusinessLayer.ValidationRules.ContactValidations;
+using BusinessLayer.ValidationRules.DiscountValidations;
+using BusinessLayer.ValidationRules.IdentityValidations;
+using BusinessLayer.ValidationRules.NotificationValidations;
+using BusinessLayer.ValidationRules.ProductValidations;
+using BusinessLayer.ValidationRules.SliderValidations;
+using BusinessLayer.ValidationRules.SocialMediaValidations;
+using BusinessLayer.ValidationRules.TableValidations;
+using BusinessLayer.ValidationRules.TestimonialValidations;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessLayer.Container
+{
+    public static class Extensions
+    {
+        public static void ContainerDependencies(this IServiceCollection services)
+        {
+            services.AddDbContext<Context>();
+
+            services.AddScoped<IAboutService, AboutManager>();
+            services.AddScoped<IAboutDal, EfAboutDal>();
+
+            services.AddScoped<IBookingService, BookingManager>();
+            services.AddScoped<IBookingDal, EfBookingDal>();
+
+            services.AddScoped<ICategoryService, CategoryManager>();
+            services.AddScoped<ICategoryDal, EfCategoryDal>();
+
+            services.AddScoped<IContactService, ContactManager>();
+            services.AddScoped<IContactDal, EfContactDal>();
+
+            services.AddScoped<IDiscountService, DiscountManager>();
+            services.AddScoped<IDiscountDal, EfDiscountDal>();
+
+            services.AddScoped<IProductService, ProductManager>();
+            services.AddScoped<IProductDal, EfProductDal>();
+
+            services.AddScoped<ISocialMediaService, SocialMediaManager>();
+            services.AddScoped<ISocialMediaDal, EfSocialMediaDal>();
+
+            services.AddScoped<ITestimonialService, TestimonialManager>();
+            services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+
+            services.AddScoped<IOrderService, OrderManager>();
+            services.AddScoped<IOrderDal, EfOrderDal>();
+
+            services.AddScoped<IMoneyCaseService, MoneyCaseManager>();
+            services.AddScoped<IMoneyCaseDal, EfMoneyCaseDal>();
+
+            services.AddScoped<ITableService, TableManager>();
+            services.AddScoped<ITableDal, EfTableDal>();
+
+            services.AddScoped<ISliderService, SliderManager>();
+            services.AddScoped<ISliderDal, EfSliderDal>();
+
+            services.AddScoped<IBasketService, BasketManager>();
+            services.AddScoped<IBasketDal, EfBasketDal>();
+
+            services.AddScoped<INotificationService, NotificationManager>();
+            services.AddScoped<INotificationDal, EfNotificationDal>();
+
+            services.AddValidatorsFromAssemblyContaining<AboutUpdateValidation>();
+            services.AddValidatorsFromAssemblyContaining<BookingAddValidation>();
+            services.AddValidatorsFromAssemblyContaining<BookingUpdateValidation>();
+            services.AddValidatorsFromAssemblyContaining<CategoryAddValidation>();
+            services.AddValidatorsFromAssemblyContaining<CategoryUpdateValidation>();
+            services.AddValidatorsFromAssemblyContaining<ContactUpdateValidation>();
+            services.AddValidatorsFromAssemblyContaining<DiscountAddValidation>();
+            services.AddValidatorsFromAssemblyContaining<DiscountUpdateValidation>();
+            services.AddValidatorsFromAssemblyContaining<RegisterValidation>();
+            services.AddValidatorsFromAssemblyContaining<NotificationAddValidation>();
+            services.AddValidatorsFromAssemblyContaining<NotificationUpdateValidation>();
+            services.AddValidatorsFromAssemblyContaining<ProductAddValidation>();
+            services.AddValidatorsFromAssemblyContaining<ProductUpdateValidation>();
+            services.AddValidatorsFromAssemblyContaining<SliderUpdateValidation>();
+            services.AddValidatorsFromAssemblyContaining<SocialMediaAddValidation>();
+            services.AddValidatorsFromAssemblyContaining<SocialMediaUpdateValidation>();
+            services.AddValidatorsFromAssemblyContaining<TableAddValidation>();
+            services.AddValidatorsFromAssemblyContaining<TableUpdateValidation>();
+            services.AddValidatorsFromAssemblyContaining<TestimonialAddValidation>();
+            services.AddValidatorsFromAssemblyContaining<TestimonialUpdateValidation>();
+        }
+    }
+}
