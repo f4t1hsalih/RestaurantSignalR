f71b142e2becc147542df7761789c68f73de5d3b
f4t1hsalih <128603000+f4t1hsalih@users.noreply.github.com>
2025-01-09 19:07:19 +0300

    Lecture 249 - Admin Panelinde son işlemler

--- UILayer/Program.cs
+++ UILayer/Program.cs
@@ -71,6 +71,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=CustomerTable}/{action=CustomerTableList}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
